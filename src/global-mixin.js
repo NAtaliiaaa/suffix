84411517b5f82a7ac2e91edaf827b0a299212398
Nataliiaaa <natalia.davudovuch@gmail.com>
2020-04-25 15:45:17 +0300

    Innitial

--- src/global-mixin.js
+++ src/global-mixin.js
@@ -1,13 +1,13 @@
 import { /*mapState,*/ mapGetters } from "vuex";
 export const mixin = {
     methods: {
-        // _getTag(categories) {
-        //     if (!this.getTagById) throw new Error("NOT_FOUND_METHOD");
-        //     const result = this.getTagById(categories[0]);
-        //     if (!result) return "";
+        _getTag(categories) {
+            if (!this.getTagById) throw new Error("NOT_FOUND_METHOD");
+            const result = this.getTagById(categories[0]);
+            if (!result) return "";
 
-        //     return result.data.category;
-        // }
+            return result.data.category;
+        }
     },
     computed: {
         ...mapGetters("blog", {
--- src/global-register.js
+++ src/global-register.js
@@ -6,12 +6,7 @@ import Vue from "vue";
  * -------------------------------
  */
 import Header from "@/chunks/header/header";
-// import Button from '@/chunks/button/button.vue';
-
-// import ServiceCard from '@/chunks/service-card/service-card.vue';
 Vue.component("v-header", Header);
-// Vue.component('v-service-card', ServiceCard);
-// Vue.component('my-button', Button);
 /**
  * -------------------------------
  * Mixins
